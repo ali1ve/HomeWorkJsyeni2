ca40e7e409fcb4a51855f05f213bc9934cb789e2
ali1ve <aliaga.yahya@gmail.com>
2024-11-04 13:58:59 +0400

    homeworkyeni2

---
+++ app.js
@@ -0,0 +1,8 @@
+let userFullName = prompt("Soyad və adınızı daxil edin")
+
+userFullName = userFullName.split(" ")
+let lastName = userFullName[0]
+let firstName = userFullName[1]
+
+console.log("Salam,"+ firstName + "!");
+
